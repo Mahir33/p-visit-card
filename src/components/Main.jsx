f16fed90692d1fc74698351e44f20022c81943f7
mahir33 <adam.marek.wrobel@gmail.com>
2024-06-25 18:51:05 +0200

    change description

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
       <div className="info">
         <h1 className="dev-name">Adam Wrobel</h1>
         <p className="profession">Frontend Developer</p>
-        <p className="website">web-link</p>
+        <p className="website">adam-wrobel-web-dev.netlify.app</p>
         <div className="btn-s">
           <button className="email-btn">Email</button>
           <button className="linkedin-btn">LinkedIn</button>
@@ -14,15 +14,15 @@ const Main = () => {
         <h2>About</h2>
         <p>
           I am a frontend developer with a particular interest in making things
-          simple and automating daily tasks. I try to keep up with security and
-          best practices, and am always looking for new things to learn.
+          simple and automating daily tasks. I try to keep up with code best
+          practices, and am always looking for new things to learn.
         </p>
       </div>
       <div className="interests">
         <h2>Interests</h2>
         <p>
-          Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
-          Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+          Coding. Singer. Travel geek. Code fanatic. Psychology. Astronomy.
+          Board games. Coffee fanatic.
         </p>
       </div>
     </main>
